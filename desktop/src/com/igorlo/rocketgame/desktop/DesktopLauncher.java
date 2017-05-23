d1b1a1985af5aa64bac59b13f6c5dd1abb3da8ad
igorlo <i440533>
2017-05-23 23:57:48 +0300

    Самая первая версия, работа над проектом только начата

---
+++ desktop/src/com/igorlo/rocketgame/desktop/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.igorlo.rocketgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.igorlo.rocketgame.RocketGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 500;
+		config.width = 800;
+		config.backgroundFPS = 60;
+		config.foregroundFPS = 60;
+		config.title = "Rocket Landing";
+		config.resizable = false;
+		new LwjglApplication(new RocketGame(), config);
+	}
+}
